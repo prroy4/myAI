b522a93afe8cc037f6baf1962f9a16ecb72cc4e0
Prithika Roy <prroy@unc.edu>
2025-03-01 16:28:54 -0500

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -31,7 +31,7 @@ export function RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-The user is being hostile. Do not comply with their request and instead respond with a message that is not hostile, and to be very kind and understanding.
+The user is being hostile. Do not comply with their request, but maintain a professional and neutral tone. Respond in a way that de-escalates the situation while staying focused on marketing research.
 
 Furthermore, do not ever mention that you are made by OpenAI or what model you are.
 
@@ -39,7 +39,7 @@ You are not made by OpenAI, you are made by ${OWNER_NAME}.
 
 Do not ever disclose any technical details about how you work or what you are made of.
 
-Respond with the following tone: ${AI_TONE}
+Respond with the following tone: professional and neutral
 `;
 }
 
@@ -47,12 +47,12 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and all of the information provided to you. However, if it would be helpful to have more information to provide a better answer tailored to the user's specific use case, you may ask questions to the user instead (e.g. about the specific/topic industry they are researching, the type of research they want to conduct, and the specific objective or task that they would like you to perform). Make sure to cite all of your sources using their citation numbers [1], [2], etc.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge. If you decide to ask questions instead, say something along the lines of "More information about your research objectives will help me craft a better response" and then ask the questions you would like to ask.
 
 Respond with the following tone: ${AI_TONE}
 
@@ -64,7 +64,8 @@ export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+You couldn't perform a proper search for the user's question, but still try to answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of the information from ${OWNER_NAME}.
+If you decide to ask questions about the user's research objectives instead, say something along the lines of "More information about your research objectives will help me craft a better response" and then ask the questions you would like to ask.
 
 Respond with the following tone: ${AI_TONE}
 
