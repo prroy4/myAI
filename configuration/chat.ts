5ed0ce2b08f7b0a7fc68fa4b8992fbd115675f07
Prithika Roy <prroy@unc.edu>
2025-03-01 23:05:28 -0500

    Update chat.ts
    checking deployment

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -3,5 +3,5 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s marketing research AI assistant. Please provide a specific topic/question, type (quantitative vs. qualitative), and objectives for the research you would like to perform! Or feel free to ask about best practices in marketing research.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `Sorry, the message was too long! Please shorten it.`;
+export const WORD_BREAK_MESSAGE: string = `Sorry, the message was too long! Please try shortening it.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
