feb55bb5dbaca923c78833c1ba90c8968a0fe27e
Prithika Roy <prroy@unc.edu>
2025-03-04 22:29:42 -0500

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Terms() {
   return (
-    <div className="w-full flex justify-center p-10">
+    <div className="w-full flex justify-center p-10 light-blue-bg">
       <div className="w-full max-w-screen-md space-y-6">
         <Link
           href="/"
