1edc785d89577c0ff79976c351444cb0709cbaa4
Prithika Roy <prroy@unc.edu>
2025-03-05 22:13:23 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Chat() {
   return (
     <>
       <ChatHeader clearMessages={clearMessages} />
-      <div className="flex justify-center items-center h-screen light-blue-bg">
-        <div className="flex flex-col max-w-screen-lg w-full h-full p-5 flex-1 light-blue-bg">
+      <div className="flex flex-col min-h-screen light-blue-bg">
+        <div className="flex flex-col max-w-screen-lg w-full flex-1 p-5">
           <ChatMessages messages={messages} indicatorState={indicatorState} />
         </div>
       </div>
